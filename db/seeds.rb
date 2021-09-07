e6001984ffe5a4258c5315d3e4e29d40d6f1777a
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-09-07 18:48:07 +0500

    some layout changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    manager1 = User.create(name: "michael", email: "michael@gmail.com", password:"password", password_confirmation: "password",user_type: "manager")
+
+    manager2 = User.create(name: "schrute", email: "schrute@gamil.com", password:"password", password_confirmation: "password",user_type: "manager")
+
+    manager3 = User.create(name: "dwight", email: "dwight@gamil.com", password:"password", password_confirmation: "password",user_type: "manager")
+ 
+    5.times do
+      manager1.projects.create(manager: manager1.name, title: Faker::Name.name)
+      manager2.projects.create(manager: manager2.name, title: Faker::Name.name)
+      manager3.projects.create(manager: manager3.name, title: Faker::Name.name)
+    end
+
+
+    dev1 = User.create(name: "kevin", email: "kevin@gmail.com", password:"password", password_confirmation: "password",user_type: "developer")
+
+    dev2 = User.create(name: "andy", email: "andy@gamil.com", password:"password", password_confirmation: "password",user_type: "developer")
+
+    dev3 = User.create(name: "ryan", email: "ryan@gamil.com", password:"password", password_confirmation: "password",user_type: "developer")
+
+    qa1 = User.create(name: "oscar", email: "oscar@gmail.com", password:"password", password_confirmation: "password",user_type: "qa")
+
+    qa2 = User.create(name: "creed", email: "creed@gamil.com", password:"password", password_confirmation: "password",user_type: "qa")
+
+    qa3 = User.create(name: "morty", email: "morty@gamil.com", password:"password", password_confirmation: "password",user_type: "qa")
